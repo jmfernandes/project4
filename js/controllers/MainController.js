64ebcdf86330d70e2a730040f31b73bfbdf19504
jmfernandes <joshfernandes@mac.com>
2017-08-13 22:58:47 -0700

    added failed option

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -3,5 +3,7 @@ app.controller('booksCtrl', function($scope, $http) {
   $http.get("https://s3-us-west-2.amazonaws.com/joshuamfernandes/california.json")
     .then(function(response) {
       $scope.data = response.data;
+    }, function(response){
+      $scope.data = response.data || 'Request failed';
     });
 });
